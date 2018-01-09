8cf476c6a9753385f71cb34a298c7b868cbdae0e
BubblingFish <1129041612@qq.com>
2018-01-09 21:45:24 +0800

    框架测试

---
+++ book/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,9 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>BookStore</title>
+	</head>
+	<body>111111111111111111
+	</body>
+</html>
