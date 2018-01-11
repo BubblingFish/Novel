c7642a5c86381398b2b4a9846a6e3680d670b137
BubblingFish <1129041612@qq.com>
2018-01-11 22:23:42 +0800

    0111

--- book/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ book/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -1,9 +1,36 @@
 <?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" charset="utf-8">
 		<title>BookStore</title>
+		<link rel="stylesheet" type="text/css" href="../../../../Public/public/bootstrap.min.css"/>
+		<style>
+			*{
+				margin: 0px;
+				padding: 0px;
+			}
+			.silde{
+				display: block;
+				width: 100%;
+				height: 500px;
+			}
+			.silder{
+				display: block;
+				width: 100%;
+				height: 100%;
+			}
+			.sildepic{
+				width: 100%;
+				height: 100%;
+			}
+		</style>
 	</head>
-	<body>111111111111111111
+	<body>
+		<div class="Content container">
+			<ul class="silde">
+				<li class="silder"><img src="../../../../Public/image/556569c40d2e5.jpg" class="sildepic"/></li>
+			</ul>
+		</div>
+		<div class="footer"></div>
 	</body>
 </html>
