29360e88619a10c4dfe063efcb629e0409b3a80f
BubblingFish <1129041612@qq.com>
2018-04-06 12:29:09 +0800

    04
    0406

--- book/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ book/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -3,33 +3,35 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>书荒之家</title>
-		<link rel="stylesheet" type="text/css" href="../../Public/styles/bootstrap.min.css" />
-		<link rel="stylesheet" type="text/css" href="../../Public/styles/nav.css" />
-		<link rel="stylesheet" type="text/css" href="../../Public/styles/logo.css" />
+		<link rel="stylesheet" type="text/css" href="../../Public/styles/1.css" />
 	</head>
 	<body>
-		<div class="container-fluid Nav">
-			<div class="row">
-				<ul class="nav_ul col-md-5">
-					<li class="nav_li"><a>首页</a></li>
-					<li class="nav_li"><a>玄幻</a></li>
-					<li class="nav_li"><a>玄幻</a></li>
-					<li class="nav_li"><a>玄幻</a></li>
-					<li class="nav_li"><a>玄幻</a></li>
-					<li class="nav_li"><a>玄幻</a></li>
-				</ul>
-				<div class="col-md-4"></div>
-				<ul class="nav_ul col-md-3">
-					<li class="nav_li"><a>登陆</a></li>
-					<li class="nav_li"><a>注册</a></li>
-				</ul>
-			</div>
-		</div>
-		
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-sm-3 Logo"></div>
-			</div>
-		</div>
+		<div class="navBox">
+			<div class="nav">
+				<ul>
+					<li class="current"><a href="#" target="_blank">书荒之家</a></li>
+                    <li><a href="#">玄幻<small>movie</small></a></li>
+                    <li><a href="#">武侠<small>tv play</small></a></li>
+                    <li><a href="#">科幻<small>comic</small></a></li>
+                    <li><a href="#">言情<small>variety</small></a></li>
+                    <li><a href="#">都市<small>documentary</small></a></li>
+                    <li><a href="#">更多<small>+</small></a></li>
+                    <li><a href="#">登陆<small>+</small></a></li>
+                    <li><a href="#">注册<small>+</small></a></li>
+               </ul>
+               <!--match IE9,IE10 or not ie-->
+               <!--[if (get IE 8) | ! (IE)]><!-->
+               <h1 class="title"><a href="#">书荒之家</a><span class="btn"><img src="../../Public/images/btn.png" width="34" height="34" alt=""/></span></h1>
+               <!--<![endif]-->
+            </div>
+       </div>
 	</body>
+	<script type="text/javascript" src="../../Public/js/jquery1.9.1.min.js"></script> 
+	<script type="text/javascript">
+		$(document).ready(function (){
+			$(".btn").on("click",function(){
+				$(".nav").find("ul").slideToggle(400);//点击滑动显示或隐藏
+				 });
+		})
+</script>
 </html>
